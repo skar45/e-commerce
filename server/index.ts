c47c2d27be1a3fe6595f08ea4fcbed649a2aac79
Sajal <karkeesajal@gmail.com>
2022-01-26 10:15:23 -0500

    bug fix

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
 import { httpServer, httpsServer } from './app';
 
-httpServer.listen(8001, () => {
+httpsServer.listen(8001, () => {
   console.log('http: Listening on port 8001!');
 });
 
-httpsServer.listen(3001, () => {
+httpServer.listen(3001, () => {
   console.log('https: Listening on port 3001!');
 });
